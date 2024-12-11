36fc1a2b3986ea634aef4ac0fa9ca7f5153cc00a
YvinecTom <tom.yvinec@etudiant.univ-brest.fr>
2024-12-11 22:23:53 +0100

    Avancement page d'accueil

--- src/main/kotlin/QuizApp.kt
+++ src/main/kotlin/QuizApp.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,15 +24,23 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
 
-val belgrano = FontFamily(
+val backgroundLight = Color(0xFFFFF8F7)
+val primaryLight = Color(0xFF8E4957)
+val primaryContainerLight = Color(0xFFFFD9DE)
+val secondaryLight = Color(0xFF75565B)
+val secondaryContainerLight = Color(0xFFFFD9DE)
+val tertiaryLight = Color(0xFF795831)
+val tertiaryContainerLight = Color(0xFFFFDDBA)
+
+val nunito = FontFamily(
     Font(
-        resource = "font/Belgrano-Regular.ttf",
+        resource = "font/Nunito-VariableFont_wght.ttf",
         weight = FontWeight.Normal
     )
 )
 // Fonction de lecture du fichier JSON
 fun readQuizQuestionsFromFile(filename: String): List<QuizQuestion> {
-    val inputStream = object {}.javaClass.getResourceAsStream("/$filename")
+    val inputStream = object {}.javaClass.getResourceAsStream("quiz/$filename")
         ?: throw IllegalArgumentException("Le fichier $filename n'a pas été trouvé dans les ressources.")
     val reader = InputStreamReader(inputStream, Charset.defaultCharset())
     return Json.decodeFromString(reader.readText())
@@ -47,13 +56,7 @@ data class QuizQuestion(
 @Preview
 @Composable
 fun QuizApp() {
-    val backgroundLight = Color(0xFFFFF8F7)
-    val primaryLight = Color(0xFF8E4957)
-    val primaryContainerLight = Color(0xFFFFD9DE)
-    val secondaryLight = Color(0xFF75565B)
-    val secondaryContainerLight = Color(0xFFFFD9DE)
-    val tertiaryLight = Color(0xFF795831)
-    val tertiaryContainerLight = Color(0xFFFFDDBA)
+
 
     var userName by remember { mutableStateOf<String?>(null) }
     var selectedCategory by remember { mutableStateOf<String?>(null) }
@@ -93,20 +96,28 @@ fun WelcomeScreen(onContinue: (String) -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         var text by remember { mutableStateOf("") }
-        Text("Bienvenue dans QuizMania !", style = MaterialTheme.typography.h4, textAlign = TextAlign.Center)
+        Text("Bienvenue dans QuizMania :", style = MaterialTheme.typography.h4, textAlign = TextAlign.Center, fontFamily = nunito)
+        Spacer(Modifier.height(16.dp))
+        Text("Veuillez entrer votre nom pour jouer !", style = MaterialTheme.typography.h4, textAlign = TextAlign.Center, fontFamily = nunito)
         Spacer(Modifier.height(16.dp))
         TextField(
             value = text,
             onValueChange = { text = it },
             label = { Text(
                 text = "Entrez votre nom",
-                fontFamily = belgrano
+                fontFamily = nunito,
+                fontSize = 15.sp
             ) },
-            modifier = Modifier.fillMaxWidth(0.8f)
+            modifier = Modifier.fillMaxWidth(0.5f),
+            textStyle = TextStyle(fontSize = 20.sp) // Taille de texte fixe
         )
         Spacer(Modifier.height(16.dp))
         Button(onClick = { if (text.isNotEmpty()) onContinue(text) }) {
-            Text("Commencer")
+            Text(
+                text = "Commencer",
+                fontFamily = nunito,
+                fontSize = 20.sp
+            )
         }
     }
 }
@@ -123,21 +134,34 @@ fun CategorySelectionScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         userName?.let {
-            Text("Bonjour $it", style = MaterialTheme.typography.h6, modifier = Modifier.padding(bottom = 16.dp))
+            Text("Bonjour $it", style = MaterialTheme.typography.h6, modifier = Modifier.padding(bottom = 25.dp), fontFamily = nunito)
         }
-        Text("Sélectionnez une catégorie", style = MaterialTheme.typography.h5, textAlign = TextAlign.Center)
+        Text("Sélectionnez une catégorie", style = MaterialTheme.typography.h5, textAlign = TextAlign.Center, fontFamily = nunito)
         Spacer(Modifier.height(16.dp))
         val categories = listOf("cinema", "jeu_video", "geographie", "series", "sport")
-        categories.forEach { category ->
-            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(8.dp)) {
-                Button(onClick = { onCategorySelected(category) }, Modifier.fillMaxWidth(0.8f)) {
-                    Text(category.capitalize())
+        Row {
+            categories.forEach { category ->
+                Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(10.dp)) {
+                    Button(
+                        onClick = { onCategorySelected(category) },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = secondaryContainerLight // Remplacez par la couleur de votre choix
+                        )
+                    ) {
+                        Text(
+                            category.capitalize(),
+                            fontFamily = nunito,
+                            color = secondaryLight,
+                            fontSize = 20.sp
+                        )
+                    }
+                    scores[category]?.let { Text("Score: $it", fontSize = 16.sp, fontFamily = nunito) }
                 }
-                scores[category]?.let { Text("Score: $it", fontSize = 16.sp) }
             }
         }
     }
 }
+
 
 @Composable
 fun QuizScreen(
@@ -159,39 +183,82 @@ fun QuizScreen(
         }
     }
 
+    // Utilisez Modifier.fillMaxSize() avec contentAlignment.Center
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundLight), // Ajoutez le fond
         contentAlignment = Alignment.Center
     ) {
         if (questions.isEmpty()) {
-            Text("Chargement des données...")
+            Text(
+                "Chargement des données...",
+                fontFamily = nunito,
+                style = MaterialTheme.typography.h6
+            )
         } else {
             val currentQuestion = questions[currentQuestionIndex]
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                userName?.let { Text("Joueur : $it", style = MaterialTheme.typography.subtitle1) }
+
+            // Utilisez Column avec verticalArrangement.Center
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth(0.9f) // Limitez la largeur
+                    .fillMaxHeight()
+                    .padding(16.dp)
+                    .background(Color.Cyan)
+            ) {
+                userName?.let {
+                    Text(
+                        "Joueur : $it",
+                        style = MaterialTheme.typography.subtitle1,
+                        fontFamily = nunito
+                    )
+                }
                 Spacer(Modifier.height(16.dp))
-                Text(currentQuestion.question, style = MaterialTheme.typography.h6)
-                Spacer(Modifier.height(16.dp))
-                Reponse(
-                    options = currentQuestion.options,
-                    currentQuestion = currentQuestion,
-                    onAnswerSelected = { selectedOption, isCorrect ->
-                        userAnswer = selectedOption
-                        message = if (isCorrect) {
-                            score++
-                            "Correct! Score: $score"
-                        } else {
-                            "Incorrect. Réponse: ${currentQuestion.answer}. Score: $score"
-                        }
-                        if (currentQuestionIndex < questions.size - 1) {
-                            currentQuestionIndex++
-                        } else {
-                            onFinishQuiz(score)
-                        }
-                    }
+                Text(
+                    currentQuestion.question,
+                    style = MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center,
+                    fontFamily = nunito
                 )
                 Spacer(Modifier.height(16.dp))
-                if (userAnswer.isNotEmpty()) Text(message)
+
+                // Centrez les réponses
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Reponse(
+                        options = currentQuestion.options,
+                        currentQuestion = currentQuestion,
+                        onAnswerSelected = { selectedOption, isCorrect ->
+                            userAnswer = selectedOption
+                            message = if (isCorrect) {
+                                score++
+                                "Correct! Score: $score"
+                            } else {
+                                "Incorrect. Réponse: ${currentQuestion.answer}. Score: $score"
+                            }
+                            if (currentQuestionIndex < questions.size - 1) {
+                                currentQuestionIndex++
+                            } else {
+                                onFinishQuiz(score)
+                            }
+                        }
+                    )
+                }
+
+                Spacer(Modifier.height(16.dp))
+
+                if (userAnswer.isNotEmpty()) {
+                    Text(
+                        message,
+                        textAlign = TextAlign.Center,
+                        fontFamily = nunito
+                    )
+                }
             }
         }
     }
